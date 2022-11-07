bb34a0437488cabffe56aa6cb741f3dde0712315
Vinicio Jimenez <vinicio.jimenez@oracle.com>
2022-11-06 22:31:28 -0600

    Lab 2

--- main.py
+++ main.py
@@ -17,99 +17,112 @@ class TwitterApp(object):
     def _create_constraints(self):
         with self.driver.session() as session:
             session.run("CREATE CONSTRAINT unique_user IF NOT EXISTS FOR (u:User) REQUIRE u.username IS UNIQUE")
-            session.run("CREATE CONSTRAINT unique_tweet IF NOT EXISTS FOR (t:Tweet) REQUIRE t.id IS UNIQUE")
-            session.run("CREATE CONSTRAINT unique_hashtag IF NOT EXISTS FOR (h:Hashtag) REQUIRE h.hashtag IS UNIQUE")
-            session.run("CREATE CONSTRAINT unique_country IF NOT EXISTS FOR (c:Country) REQUIRE c.name IS UNIQUE")
-            #session.run("CREATE CONSTRAINT unique_used_hashtag_timestamp IF NOT EXISTS FOR ()-[r:USED_HASHTAG]-() REQUIRE r.timestamp IS NOT NULL")
+            session.run("CREATE CONSTRAINT unique_subreddit IF NOT EXISTS FOR (s:Subreddit) REQUIRE s.name IS UNIQUE")
 
-
-    def _create_user_node(self, username):
+    def _create_user_node(self, username, followers, karma):
         with self.driver.session() as session:
             try:
-                session.run("CREATE (u:User {username: $username})", username=username)
+                session.run("CREATE (u:User {username: $username, num_of_followers: $followers, karma: $karma})", username=username, followers=followers, karma=karma)
+            except ConstraintError:
+                print("User already exists: ", username)
+
+    def _create_subreddit_node(self, name, description,subscribers):
+        with self.driver.session() as session:
+            try:
+                session.run("CREATE (s:Subreddit {name: $name, description: $description, total_subscribers: $subscribers})", name=name, description=description,subscribers=subscribers)
+            except ConstraintError:
+                print("Subreddit already exists: ", name)
+
+    def _create_post_node(self, title, text, karma):
+        with self.driver.session() as session:
+            try:
+                session.run("CREATE (p:Post {title: $title, text: $text, karma: $karma})", title=title,text=text,karma=karma)
             except ConstraintError:
                 pass
 
-    def _create_tweet_node(self, _id, tweet):
+    def _create_comment_node(self, text,karma):
         with self.driver.session() as session:
             try:
-                session.run("CREATE (t:Tweet {_id: $_id, tweet: $tweet})", _id=_id, tweet=tweet)
+                session.run("CREATE (c:Comment {text: $text, karma: $karma, timestamp: $timestamp})", text=text,karma=karma)
             except ConstraintError:
                 pass
 
-    def _create_country_node(self, country):
-        with self.driver.session() as session:
-            try:
-                session.run("CREATE (c:Country {name: $country})", country=country)
-            except ConstraintError:
-                pass
-
-    def _create_hashtag_node(self, hashtag):
-        with self.driver.session() as session:
-            try:
-                session.run("CREATE (h:Hashtag {hashtag: $hashtag})", hashtag=hashtag)
-            except ConstraintError:
-                pass
-
-    def _create_user_to_hashtag_relationship(self, username, hashtag, timestamp):
+    def _create_subscribes_relationship(self, username, subreddit):
         with self.driver.session() as session:
             session.run("""
-                MATCH (u:User), (h:Hashtag)
-                WHERE u.username=$username AND h.hashtag=$hashtag
-                CREATE (u)-[r:USED_HASHTAG {timestamp: $timestamp}]->(h)
-                RETURN type(r)""", username=username, hashtag=hashtag, timestamp=timestamp)
+                MATCH (u:User), (s:Subreddit)
+                WHERE u.username=$username AND s.name=$subreddit
+                CREATE (u)-[r:SUBSCRIBES]->(s)
+                RETURN type(r)""", username=username, subreddit=subreddit)
 
 
-    def _create_user_to_user_relationship(self, username, mentioned, timestamp):
+    def _create_moderates_relationship(self, username, subreddit):
         with self.driver.session() as session:
             session.run("""
-                MATCH (u1:User), (u2:User)
-                WHERE u1.username=$username AND u2.username=$mentioned
-                CREATE (u1)-[r:MENTIONED {timestamp: $timestamp}]->(u2)
-                RETURN type(r)""", username=username, mentioned=mentioned, timestamp=timestamp)
+                MATCH (u:User), (s:Subreddit)
+                WHERE u.username=$username AND s.name=$subreddit
+                CREATE (u)-[r:MODERATES]->(s)
+                RETURN type(r)""", username=username, subreddit=subreddit)
 
-    def _create_user_to_country_relationship(self, username, country):
+    def _create_published_relationship(self, username, post):
         with self.driver.session() as session:
             session.run("""
-                MATCH (u:User), (c:Country)
-                WHERE u.username=$username AND c.name=$country
-                CREATE (u)-[r:FROM]->(c)
-                RETURN type(r)""", username=username, country=country)
+                MATCH (u:User), (p:Post)
+                WHERE u.username=$username AND p.title=$post
+                CREATE (u)-[r:FROM]->(p)
+                RETURN type(r)""", username=username, post=post)
 
-    def _create_user_to_tweet_relationship(self, username, _id, timestamp):
+    def _create_downvotes_relationship(self, username, post):
         with self.driver.session() as session:
             session.run("""
-                MATCH (u:User), (t:Tweet)
-                WHERE u.username=$username AND t._id=$_id
-                CREATE (u)-[r:TWEETED {timestamp: $timestamp}]->(t)
-                RETURN type(r)""", username=username, _id=_id, timestamp=timestamp)
+                MATCH (u:User), (p:Post)
+                WHERE u.username=$username AND p.title=$post
+                CREATE (u)-[r:DOWNVOTES]->(p)
+                RETURN type(r)""", username=username, post=post)
+
+    def _create_upvotes_relationship(self, username, post):
+        with self.driver.session() as session:
+            session.run("""
+                MATCH (u:User), (p:Post)
+                WHERE u.username=$username AND p.title=$post
+                CREATE (u)-[r:UPVOTES]->(p)
+                RETURN type(r)""", username=username, post=post)
+
+    def _create_commented_relationship(self, username, comment):
+        with self.driver.session() as session:
+            session.run("""
+                MATCH (u:User), (c:Comment)
+                WHERE u.username=$username1 AND c.text=$comment
+                CREATE (u)-[r:COMMENTED]->(c)
+                RETURN type(r)""", username=username, comment=comment)
 
     def init(self, source):
         with open(source, newline='') as csv_file:
-            reader = csv.DictReader(csv_file,  delimiter='|')
+            reader = csv.DictReader(csv_file,  delimiter=',')
             for r in reader:
-                self._create_user_node(r["handle"])
-                self._create_tweet_node(r["tweet_id"], r["tweet"])
-                self._create_country_node(r["country"])
-                tweet_tokens = r["tweet"].split()
-                for token in tweet_tokens:
-                    if token.startswith("#"):
-                        self._create_hashtag_node(token)
-                        self._create_user_to_hashtag_relationship(r["handle"], token, r["date"])
-                    if token.startswith('@'):
-                        self._create_user_node(token)
-                        self._create_user_to_user_relationship(r["handle"], token, r["date"])
-                self._create_user_to_tweet_relationship(r["handle"], r["tweet_id"], r["date"])
-                self._create_user_to_country_relationship(r["handle"], r["country"])
+                self._create_user_node(r["username"],r["followers"],r["user_karma"])
+                self._create_subreddit_node(r["subreddit_name"], r["description"],r["subscribers"])
+                self._create_subscribes_relationship(r["username"], r["subreddit_name"])
+                self._create_moderates_relationship(r["username"], r["subreddit_name"])
+                if r["type"] == 'Post':
+                    self._create_post_node(r["title"],r["post_text"],r["post_karma"])
+                    self._create_published_relationship(r["username"], r["title"])
+                if r["type"] == 'Comment':
+                    self._create_comment_node(r["comment_text"],r["comment_karma"])
+                    self._create_commented_relationship(r["username"], r["comment_text"])
+                if r["type"] == 'Upvote':
+                    self._create_downvotes_relationship(r["username"], r["title"])
+                if r["type"] == 'Downvote':
+                    self._create_upvotes_relationship(r["username"], r["title"])
 
 if __name__ == "__main__":
     # Read connection env variables
     neo4j_uri = os.getenv('NEO4J_URI', 'bolt://localhost:7687')
     neo4j_user = os.getenv('NEO4J_USER', 'neo4j')
-    neo4j_password = os.getenv('NEO4J_PASSWORD', 'eternal-food-ford-quiz-visitor-5127')
+    neo4j_password = os.getenv('NEO4J_PASSWORD', 'T4t13584#')
 
     twitter = TwitterApp(neo4j_uri, neo4j_user, neo4j_password)
-    twitter.init("data/tweets.csv")
+    twitter.init("data/reddit.csv")
 
     twitter.close()
 
